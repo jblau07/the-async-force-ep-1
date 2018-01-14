780e79b16ed65259fd3e3d8dbd5c29ad0c4f1834
jblau07 <jlau7248@gmail.com>
2018-01-13 15:09:29 -1000

    person 14 home and species finished

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ function fillIn4() {
   let person4 = JSON.parse(this.response);
   document.getElementById("person4Name").innerHTML = person4.name
 
-  let oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", fillInHome4);
-  oReq.open("GET", person4.homeworld);
-  oReq.send();
+  let gReq = new XMLHttpRequest();
+  gReq.addEventListener("load", fillInHome4);
+  gReq.open("GET", person4.homeworld);
+  gReq.send();
 }
 
 let hReq = new XMLHttpRequest();
@@ -16,4 +16,24 @@ hReq.send();
 function fillInHome4() {
   let person4Home = JSON.parse(this.response);
   document.getElementById("person4HomeWorld").innerHTML = person4Home.name;
+}
+
+function fillIn14(){
+  let person14 = JSON.parse(this.response);
+  document.getElementById("person14Name").innerHTML = person14.name;
+
+  let iReq = new XMLHttpRequest();
+  iReq.addEventListener("load", fillInSpecies14);
+  iReq.open("GET", person14.species);
+  iReq.send();
+}
+
+let jReq = new XMLHttpRequest();
+jReq.addEventListener("load", fillIn14);
+jReq.open("GET", "https://swapi.co/api/people/14/");
+jReq.send();
+
+function fillInSpecies14(){
+  let person14Home = JSON.parse(this.response);
+  document.getElementById("person14Species").innerHTML = person14Home.name;
 }
