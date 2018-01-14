efeedfe41cd3ab783951d337ce62e388d653fbdb
jblau07 <jlau7248@gmail.com>
2018-01-13 14:57:29 -1000

    person 4 name and homeworld work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
-
-function fillIn (){
-  //console.log(this.responseText);
+function fillIn4() {
   let person4 = JSON.parse(this.response);
   document.getElementById("person4Name").innerHTML = person4.name
-}
-let oReq = new XMLHttpRequest();
-oReq.addEventListener("load", fillIn);
-oReq.open("GET", "https://swapi.co/api/people/4/")
-oReq.send();
 
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", fillInHome4);
+  oReq.open("GET", person4.homeworld);
+  oReq.send();
+}
+
+let hReq = new XMLHttpRequest();
+hReq.addEventListener("load", fillIn4);
+hReq.open("GET", "https://swapi.co/api/people/4/")
+hReq.send();
+
+function fillInHome4() {
+  let person4Home = JSON.parse(this.response);
+  document.getElementById("person4HomeWorld").innerHTML = person4Home.name;
+}
